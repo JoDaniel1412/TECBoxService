57e1acf8ecf3f1457ecaca35d99e13e761231f7b
Jose Acuña <jodaniel1409@gmail.com>
2020-03-09 22:15:26 -0600

    Added DataBaseManager

--- DB/DataBaseManager.cs
+++ DB/DataBaseManager.cs
@@ -4,10 +4,18 @@ using TECBoxService.Models;
 
 namespace TECBoxService.DB
 {
-    public class DataBaseManager
+    // Class to deserialize the DB to Modules
+    public static class DataBaseManager
     {
         public static List<Employee> GetEmployees() => Deserialize<List<Employee>>("Employee.json");
-        
-        private static T Deserialize<T>(string file) => Newtonsoft.Json.JsonConvert.DeserializeObject<T>(file);
+        public static List<Seller> GetSellers() => Deserialize<List<Seller>>("Seller.json");
+        public static List<Client> GetClient() => Deserialize<List<Client>>("Client.json");
+        public static List<Place> GetPlace() => Deserialize<List<Place>>("Place.json");
+        public static List<Subsidiary> GetSubsidiary() => Deserialize<List<Subsidiary>>("Subsidiary.json");
+        public static List<Bill> GetBill() => Deserialize<List<Bill>>("Bill.json");
+        public static List<Route> GetRoute() => Deserialize<List<Route>>("Route.json");
+        public static List<Product> GetProduct() => Deserialize<List<Product>>("Product.json");
+        public static List<Package> GetPackage() => Deserialize<List<Package>>("Package.json");
+        public static T Deserialize<T>(string file) => Newtonsoft.Json.JsonConvert.DeserializeObject<T>(file);
     }
 }

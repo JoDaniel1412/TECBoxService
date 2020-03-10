6e5cb552c1ffcca45519dc7dfecfc33da01b4470
Jose Acuña <jodaniel1409@gmail.com>
2020-03-09 20:02:54 -0600

    Added Employee model and controller as examples

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TECBoxService.Models;
+
+namespace TECBoxService.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        List<Employee> employees = new List<Employee>();
+
+        public EmployeeController()
+        {
+        }
+
+        // GET api/<controller>
+        public List<Employee> Get()
+        {
+            return employees;
+        }
+
+        // GET api/<controller>/5
+        public Employee Get(int id)
+        {
+            return employees.Where(x => x.ID == id).FirstOrDefault();
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]Employee value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]Employee value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TECBoxService.Models
+{
+    public class Employee : Person
+    {
+
+        public long SubsidiaryId { get; set; }
+        public string Birthdate { get; set; }
+        public string Role { get; set; }
+        public string SalaryHour { get; set; }
+        public int WorkedHours { get; set; }
+        public int SalaryMonth { get; set; }
+    }
+}

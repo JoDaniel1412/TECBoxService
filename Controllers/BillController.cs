61c9727d859fd7bde3919d26d791d92eb2a9a4b9
Jose Acuña <jodaniel1409@gmail.com>
2020-03-09 22:31:00 -0600

    Added Controllers

---
+++ Controllers/BillController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using TECBoxService.DB;
+using TECBoxService.Models;
+
+namespace TECBoxService.Controllers
+{
+    public class BillController : ApiController
+    {
+        // GET api/<controller>
+        public List<Bill> Get()
+        {
+            return DataBaseManager.GetBill();
+        }
+
+        // GET api/<controller>/5
+        public Bill Get(int id)
+        {
+            return DataBaseManager.GetBill().FirstOrDefault(x => x.Id == id);
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]Bill value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]Bill value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}

2bc0cdcec041c16299833857fbda641ab688140b
Jose Acuña <jodaniel1409@gmail.com>
2020-03-09 21:15:25 -0600

    Initiated the  DB Manager

---
+++ DB/DataBaseManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using TECBoxService.Models;
+
+namespace TECBoxService.DB
+{
+    public class DataBaseManager
+    {
+        public static List<Employee> GetEmployees() => Deserialize<List<Employee>>("Employee.json");
+        
+        private static T Deserialize<T>(string file) => Newtonsoft.Json.JsonConvert.DeserializeObject<T>(file);
+    }
+}

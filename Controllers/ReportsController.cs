8defc80f23918f42b4f12d0478a4a5cf8ce87edc
Jose Acuña <jodaniel1409@gmail.com>
2020-03-15 06:43:17 -0600

    Implemented Crystal Reports

---
+++ Controllers/ReportsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace TECBoxService.Controllers
+{
+    public class ReportsController : ApiController
+    {
+        // GET api/<controller>
+        public void Get()
+        {
+            DB.ReportsXML.GenerateTop25Products();
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+    }
+}
---
+++ DB/ReportsXML.cs
@@ -0,0 +1,38 @@
+﻿using MoreLinq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+using TECBoxService.Models;
+
+namespace TECBoxService.DB
+{
+    public class ReportsXML
+    {
+        public static void GenerateTop25Products()
+        {
+            List<Product> top25 = new List<Product>();
+            var products = DataBaseManager.GetProduct();
+            while (top25.Count < 25 && products.Count > 0)
+            {
+                Product top = products.MaxBy(t => t.Sells).First();
+                top25.Add(top);
+                products.Remove(top);
+            }
+
+            GenerateXML<Product>(top25, "Top25.xml");
+        }
+
+        private static void GenerateXML<T>(List<T> serializable, string filename)
+        {
+            const string subFolder = "DB";
+            var rPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, subFolder, filename);
+            XmlSerializer serializer = new XmlSerializer(typeof(List<T>));
+            using (TextWriter writer = new StreamWriter(rPath))
+            {
+                serializer.Serialize(writer, serializable);
+            }
+        }
+    }
+
+}
